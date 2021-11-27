ad661e11620239f1d7a01d4e1392bb5ad886009b
peanutzhen <astzls213@163.com>
2021-11-27 18:58:35 +0800

    optimize code

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+const FilepathMeta = "meta.json"
+
+const (
+	ApiStuInfo = "https://stuhealth.jnu.edu.cn/api/user/stuinfo"
+	ApiMain    = "https://stuhealth.jnu.edu.cn/api/write/main"
+)
+
+const (
+	HeaderHost        = "stuhealth.jnu.edu.cn"
+	HeaderContentType = "application/json"
+	HeaderUserAgent   = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+)
